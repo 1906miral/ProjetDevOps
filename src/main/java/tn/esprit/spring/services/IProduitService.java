6383432494c7213cadb684f86d9d1066836a4371
Miral Barhoumi <47795367+1906miral@users.noreply.github.com>
2022-10-30 00:46:23 +0100

    Delete src directory

--- src/main/java/tn/esprit/spring/services/IProduitService.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.services;
-
-import java.util.Date;
-
-public interface IProduitService {
-     void assignProduitToStock(Long idProd,Long idStock);
-     //float getRevenuBrutProd(Long idProd,Date startDate,Date endDate);
-    
-}
