db5aaac047b74b27ffed8da311e78293d3446ce6
Miral Barhoumi <47795367+1906miral@users.noreply.github.com>
2022-11-12 17:52:36 +0100

    Update CategorieProduitController.java

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -9,6 +9,7 @@ import tn.esprit.rh.achat.services.ICategorieProduitService;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @Api(tags = "Gestion Catégories produits")
 @RequestMapping("/categorieProduit")
 public class CategorieProduitController {
